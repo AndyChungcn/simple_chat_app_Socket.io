cb1bb497655a514ca4d3365838486b66a0c051af
andychung <andychung1005@gmail.com>
2019-02-17 23:26:13 +0800

    add form for messages and show incoming messages in browser

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,21 +1,25 @@
 var socket = io();
 socket.on("connect", function() {
   console.log("socket connected to server!");
-
-  // emit newEmail event
-
-  socket.emit("createMessage", {
-    author: "andychung",
-    text: "how are you dude?"
-  });
 });
 
 socket.on("disconnect", function() {
   console.log("socket disconnected!");
 });
 
-// listen to newEmail event
+// listen event
 
 socket.on("newMessage", function(message) {
   console.log("new message: ", message);
+  var li = `<li>from: ${message.from} mes: ${message.text}</li>`;
+  $("#showMessages").append(li);
+});
+
+$("#form-selector").on("submit", e => {
+  e.preventDefault();
+
+  socket.emit("createMessage", {
+    from: $("[name=userName]").val(),
+    text: $("[name=message]").val()
+  });
 });
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const { generateMessage } = require("./utils/message");
+
 io.on("connection", socket => {
   console.log("socket connected!!");
 
@@ -14,27 +16,10 @@ io.on("connection", socket => {
     console.log("user was disconnected!");
   });
 
-  socket.emit("newMessage", {
-    author: "andy",
-    message: "welcome to this channel",
-    createdAt: new Date().getTime()
-  });
-
-  socket.broadcast.emit("newMessage", {
-    author: "andy",
-    message: "welcome to my channel broadcast",
-    createdAt: new Date().getTime()
-  });
-
   // listen event
-  socket.on("createMessage", message => {
-    console.log("new message: ", message);
-    console.log("create message");
-    socket.broadcast.emit("newMessage", {
-      author: message.author,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+  socket.on("createMessage", (message, callback) => {
+    // emit event
+    io.emit("newMessage", generateMessage(message.from, message.text));
   });
 });
 
