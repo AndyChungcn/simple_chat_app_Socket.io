94bd4e1296a421edcd35b8b0f41b9c6f8eaff7b5
andychung <andychung1005@gmail.com>
2019-02-17 20:57:47 +0800

    create express app

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(path.join(__dirname, "../public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, () => {
   console.log(`server start on port ${PORT}`);
