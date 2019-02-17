0855354b7e155da73238ecc46578551960a2ab46
andychung <andychung1005@gmail.com>
2019-02-17 21:17:51 +0800

    integrate socket.io into app

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", socket => {
+  console.log("socket connected!!");
+
+  socket.on("disconnect", () => {
+    console.log("user was disconnected!");
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server start on port ${PORT}`);
 });
